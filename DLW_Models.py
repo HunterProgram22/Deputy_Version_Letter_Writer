be14368e94d2d5317dd4b19350dee0c008a62c38
Justin Kudela <JustinKudela@gmail.com>
2017-10-12 12:17:22 -0400

    Combined Templates file into Models file.

--- DLW_Models.py
+++ DLW_Models.py
@@ -1,7 +1,27 @@
 from tkinter import StringVar, IntVar, Text
-import time
+import time, docx, os
+
+AOD_FILEPATH = 'S:\\AOD\\2017'
+GEN_FILEPATH = 'S:\\Deputy Clerks'
+PATH = 'S:\\AOD\\AOD_Test\\'
+TEMPLATE_PATH = "S:\\Letter_Writer\\Templates\\"
+SAVE_PATH = "S:\\Letter_Writer\\"
+
+TEMPLATE = TEMPLATE_PATH + 'Template.docx'
+AFFIANT_TEMPLATE = TEMPLATE_PATH + 'Affiant_Template.docx'
+JUDGE_TEMPLATE = TEMPLATE_PATH + 'Judge_Template.docx'
+GEN_TEMPLATE = TEMPLATE_PATH + 'Gen_Template.docx'
+NOTFILED_TEMPLATE = TEMPLATE_PATH + 'NotFiled_Template.docx'
+NOCASE_TEMPLATE = TEMPLATE_PATH + 'NoCase_Template.docx'
+NOFORMS_TEMPLATE = TEMPLATE_PATH + 'NoForms_Template.docx'
+LATEJUR_TEMPLATE = TEMPLATE_PATH + 'LateJur_Template.docx'
+LATEJUR_DAF_TEMPLATE = TEMPLATE_PATH + 'LateJurDelayedAppeal_Template.docx'
+JUR_MISSINGDOCS_TEMPLATE = TEMPLATE_PATH + 'JurMissingDocs_Template.docx'
+DAF_NOFACTS_TEMPLATE = TEMPLATE_PATH + 'DafNoFacts_Template.docx'
+DAF_NOOPINION_TEMPLATE = TEMPLATE_PATH + 'DafNoOpinion_Template.docx'
 
 DATE_LETTER = time.strftime("%B %d, %Y")
+
 
 class Address(object):
     """ A for creating a mailing address. """
@@ -160,3 +180,164 @@ class AODRequirements(Requirements):
             if item[1].get() == 1:
                 self.affiant_body_block.append(item[2])
         return self.affiant_body_block
+
+
+class Letter(object):
+    def __init__(self, fields):
+        self.path = SAVE_PATH
+        self.fields = fields
+        self.letter = docx.Document(TEMPLATE)
+        for paragraph in self.letter.paragraphs:
+            p = paragraph._element
+            p.getparent().remove(p)
+            p._p = p._element = None
+
+    def create_letter(self):
+        """self.template is not initialized for superclass, in the
+        subclasses it is assigned the relevant template."""
+        self.master_template = docx.Document(self.template)
+        self.string = ""
+        for paragraph in self.master_template.paragraphs:
+            self.string = self.string + '\n' + paragraph.text
+        self.newstring = self.string.format(**self.fields)
+        self.letter.add_paragraph(self.newstring)
+        self.letter.save(self.docname)
+        self.open_letter()
+
+    def open_letter(self):
+        os.startfile(self.docname)
+
+
+class AOD_AffiantLetter(Letter):
+    def __init__(self, fields):
+        Letter.__init__(self, fields)
+        self.template = AFFIANT_TEMPLATE
+        self.docname = self.path + self.fields["affiant_last_name"] + ', ' +\
+            self.fields["affiant_first_name"] + " (" + self.fields["date"] + ")" +\
+            '.docx'
+
+    def create_letter(self):
+        """Need to modify this to put rejection reasons in an indented
+        bullet list format. String, then Docx Add Paragraph, then String.
+        Or if fix is done elsewhere eliminate this method."""
+        self.master_template = docx.Document(self.template)
+        self.string = ""
+        for paragraph in self.master_template.paragraphs:
+            self.string = self.string + '\n' + paragraph.text
+        self.newstring = self.string.format(**self.fields)
+        self.letter.add_paragraph(self.newstring)
+        self.letter.save(self.docname)
+        self.open_letter()
+
+
+class AOD_JudgeLetter(Letter):
+    def __init__(self, fields):
+        Letter.__init__(self, fields)
+        self.template = JUDGE_TEMPLATE
+        self.docname = self.path + self.fields["judge_last_name"] + ', ' +\
+            self.fields["judge_first_name"] + " (" + self.fields["date"] + ")" +\
+            '.docx'
+
+
+class GEN_Letter(Letter):
+    def __init__(self, fields):
+        Letter.__init__(self, fields)
+        self.fields = fields
+        self.template = GEN_Letter.get_template()
+        self.docname = self.path + self.fields["Last Name"] + ', ' +\
+            self.fields["First Name"] + " (" + self.fields["Date"] + ")" +\
+            '.docx'
+
+    @classmethod
+    def get_template(cls):
+        return GEN_TEMPLATE
+
+    @classmethod
+    def return_preview(cls):
+        preview_template = docx.Document(cls.get_template())
+        string = ""
+        for paragraph in preview_template.paragraphs:
+            string = string + '\n' + paragraph.text
+        new = string.split(':', maxsplit=1)
+        new = new[1].split('\n', maxsplit=2)
+        return new[2:]
+
+
+class GEN_NotFiledLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = NOTFILED_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return NOTFILED_TEMPLATE
+
+
+class GEN_NoCaseLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = NOCASE_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return NOCASE_TEMPLATE
+
+
+class GEN_NoFormsLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = NOFORMS_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return NOFORMS_TEMPLATE
+
+
+class JUR_LateJurLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = LATEJUR_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return LATEJUR_TEMPLATE
+
+
+class JUR_LateJurDelayedAppealLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = LATEJUR_DAF_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return LATEJUR_DAF_TEMPLATE
+
+
+class JUR_TimelyJurMissingDocsLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = JUR_MISSINGDOCS_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return JUR_MISSINGDOCS_TEMPLATE
+
+
+class DAF_NoOpinionLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = DAF_NOOPINION_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return DAF_NOOPINION_TEMPLATE
+
+
+class DAF_NoFactsAffLetter(GEN_Letter):
+    def __init__(self, fields):
+        GEN_Letter.__init__(self, fields)
+        self.template = DAF_NOFACTS_TEMPLATE
+
+    @classmethod
+    def get_template(cls):
+        return DAF_NOFACTS_TEMPLATE
